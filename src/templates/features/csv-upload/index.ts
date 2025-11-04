f48f9796f1dd5d557ec951459dc4ef6084e3a2f2
David Aguirre <42009518+Hermes08@users.noreply.github.com>
2025-11-04 00:31:32 -0500

    feat(templates): scaffold feature modules system for MVP generator

---
+++ src/templates/features/csv-upload/index.ts
@@ -0,0 +1,12 @@
+/**
+ * CSV Upload Feature Module
+ * 
+ * Export all public components and types from this module
+ */
+
+export { CSVUpload, default } from './components/CSVUpload';
+
+// Future exports can be added here:
+// export * from './types';
+// export * from './hooks';
+// export * from './utils';
